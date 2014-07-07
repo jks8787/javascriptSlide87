89f2e7424bd8d4299777ad9b45d4e44e965b8bc4
Janice Smith <janice.k.n.smith@gmail.com>
2014-07-06 21:06:08 -0400

    optimized code - made it more dynamic

--- main.js
+++ main.js
@@ -1,37 +1,72 @@
-$(document).ready(function(){
-  var currentPageI = -1;
-  var pages = [
-      $('div.l1'),
-      $('div.l2'),
-      $('div.l3'),
-  ];
-  var viewsWidth = 200;
-  var showPage = function(index){
-      if(index === currentPageI){return;}
-      var currentPage = pages[currentPageI];
-      if(currentPage){
-          currentPage.stop().animate({left: -viewsWidth})
-      }
-      var nextPage = pages[index];
-      nextPage
-          .stop()
-          .css({left: viewsWidth})
-          .animate({left: 0})
-      currentPageI = index;
-  }
-  // show default page
-  showPage(0);
-  $('a.l1').click(showPage.bind(null, 0));
-  $('a.l2').click(showPage.bind(null, 1));
-  $('a.l3').click(showPage.bind(null, 2));
+var currentPageI = -1;
 
-  $('.left-right').mouseover(function(){
+// Get Content
+var $slideshowContent = $('.slideshow-content');
+
+// Get Nav
+var $slideshowNav = $('.slideshow-nav')
+
+// Get the contents children
+var pages = $slideshowContent.children();
+
+// Make each child a jquery object
+// and create the navigation
+pages.each(function(index, domElement){
+  var $anchor;
+
+  pages[index] = $(domElement);
+
+  $anchor = $('<a />',{
+    class: 'l' + (index + 1)
+  });
+
+  // TODO: Put generated anchors in array
+  // once this is done, append all anchors at once
+  $slideshowNav.append($anchor);
+});
+
+var viewsWidth = 200;
+var showPage = function(index){
+  var currentPage,
+      nextPage;
+
+    if(index === currentPageI) {
+      return;
+    }
+
+    currentPage = pages[currentPageI];
+
+    if(currentPage){
+        currentPage.stop().animate({left: -viewsWidth})
+    }
+
+    nextPage = pages[index];
+
+    nextPage
+        .stop()
+        .css({left: viewsWidth})
+        .animate({left: 0});
+
+    currentPageI = index;
+};
+
+$slideshowNav.on('click', function(e){
+  var index = $(e.target).index();
+
+  showPage(index);
+});
+
+$('.left-right').
+  mouseover(function(){
       $('.slider').stop().animate({
           right: 0
       }, 400);
-  }).mouseout(function(){
-      $('.slider').stop().animate({
-          right: '-200px'
-      }, 400);
+  }).
+  mouseout(function(){
+    $('.slider').stop().animate({
+        right: '-200px'
+    }, 400);
   });
-});
+
+// show default page
+showPage(0);
