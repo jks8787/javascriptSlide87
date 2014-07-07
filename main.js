610a6ff23415a125510172441719252d2d7b1853
Janice Smith <janice.k.n.smith@gmail.com>
2014-07-06 21:36:13 -0400

    working on changing application of slide

--- main.js
+++ main.js
@@ -1,72 +1,56 @@
-var currentPageI = -1;
+// Slider Engine
+var curIndex = -1;
 
 // Get Content
-var $slideshowContent = $('.slideshow-content');
-
-// Get Nav
-var $slideshowNav = $('.slideshow-nav')
+var $slideshowContentList = $('.slideshow-content-list');
 
 // Get the contents children
-var pages = $slideshowContent.children();
+var pages = $slideshowContentList.children();
 
 // Make each child a jquery object
-// and create the navigation
 pages.each(function(index, domElement){
-  var $anchor;
-
   pages[index] = $(domElement);
-
-  $anchor = $('<a />',{
-    class: 'l' + (index + 1)
-  });
-
-  // TODO: Put generated anchors in array
-  // once this is done, append all anchors at once
-  $slideshowNav.append($anchor);
 });
 
 var viewsWidth = 200;
-var showPage = function(index){
+var showNexSlide = function(index){
   var currentPage,
-      nextPage;
+  nextPage
+  index = curIndex;
 
-    if(index === currentPageI) {
-      return;
-    }
+  if (curIndex === -1) {
+    index = 0;
+  }
 
-    currentPage = pages[currentPageI];
+  currentPage = pages[index];
 
-    if(currentPage){
-        currentPage.stop().animate({left: -viewsWidth})
-    }
+  if(currentPage){
+      currentPage.stop().animate({
+        left: -viewsWidth
+      })
+  }
 
-    nextPage = pages[index];
+  nextPage = pages[index];
 
-    nextPage
-        .stop()
-        .css({left: viewsWidth})
-        .animate({left: 0});
+  nextPage
+      .stop()
+      .css({
+        left: viewsWidth
+      })
+      .animate({
+        left: 0
+      });
 
-    currentPageI = index;
+  curIndex = ++index;
 };
 
-$slideshowNav.on('click', function(e){
-  var index = $(e.target).index();
 
-  showPage(index);
+// Navigation Engine
+var $slideshowContent = $('.slideshow-content');
+
+$slideshowContent.on('click', 'a', function(){
+  showNexSlide();
 });
 
-$('.left-right').
-  mouseover(function(){
-      $('.slider').stop().animate({
-          right: 0
-      }, 400);
-  }).
-  mouseout(function(){
-    $('.slider').stop().animate({
-        right: '-200px'
-    }, 400);
-  });
-
 // show default page
-showPage(0);
+showNexSlide();
